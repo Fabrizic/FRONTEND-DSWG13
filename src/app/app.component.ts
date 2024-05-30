c274d61506d5d49c7472719f70540207569191d7
inwe-research-group <zmamanir@unmsm.edu.pe>
2024-05-29 21:40:03 -0500

    add CUS RegistrarPersona and validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistrarPersonaComponent } from './registrar-persona/registrar-persona.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [RouterOutlet, RegistrarPersonaComponent],
 })
 export class AppComponent {
   title = 'angular-demo01-241D';
   formIsInvalid = true;
+  testId = 'main-cta';
 }
---
+++ src/app/service/Persona.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Persona } from '../model/Persona';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PersonaService {
+  readonly BASE_URL: string = 'http://localhost:5000/api/v1/personas';
+  //'http://localhost:8080/api/v1/personas';
+  //'https://sigcon-backend.onrender.com/api/v1/personas';
+
+  constructor(private http: HttpClient) {}
+
+  getPersonas(): Observable<Persona[]> {
+    return this.http.get<Persona[]>(`${this.BASE_URL}/listar`);
+  }
+  registrarPersona(form: any) {
+    return this.http.post(`${this.BASE_URL}/agregar`, form);
+  }
+  //agregado para soporte flask
+  actualizarPersona(form: any) {
+    return this.http.post(`${this.BASE_URL}/actualizar`, form);
+  }
+
+  eliminarPersona(persona: Persona) {
+    return this.http.delete(`${this.BASE_URL}/eliminar`, {
+      body: persona,
+    });
+  }
+}
